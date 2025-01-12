0464d4a89927e6576c219efd0268d2876a6d67d9
Zero_Xilo <52717793+zeroxilo@users.noreply.github.com>
2025-01-12 21:15:14 +0800

    style重定向

--- Generator/2024style定向.py
+++ Generator/2024style定向.py
@@ -1,12 +1,29 @@
 import os
-from 文件遍历 import walk_through_files
+from 文件遍历 import get_base_path,walk_through_files
 
-rename_list: list = []
+base_path = get_base_path()
 
-def record_rename_as(file_path,file_name):
-
+def fix_style_link(file_path,file_name):
+    data = ""
+    with open(file_path,mode="r",encoding="gbk") as _f:
+        data = _f.read()
+    if "<link rel=" in data and "style.css\">" in data:
+        left = data.find("<link rel=")+29
+        right = data.find("style.css\">")
+        #print(data[:left]+"xxx"+data[right:])
+        #获取深度
+        relpath = os.path.relpath(file_path,base_path)
+        depth = relpath.count("\\")
+        print(relpath+"："+str(depth))
+        middle_text = "./"
+        if depth == 2:
+            middle_text = "../"
+        elif depth == 3:
+            middle_text = "../../"
+        data = data[:left]+middle_text+data[right:]
+        with open(file_path,mode="w",encoding="gbk") as _f:
+            _f.write(data)
+    #<link rel="stylesheet" href="../style.css">
 
 if __name__ == "__main__":
-    print("处理开始")
-    walk_through_files(record_rename_as,"玩家手册2024")
-    start_renaming()
+    walk_through_files(fix_style_link,"玩家手册2024")
--- Generator/文件遍历.py
+++ Generator/文件遍历.py
@@ -1,21 +1,29 @@
 import os
 import re
 
+def get_base_path():
+    """
+    获取工程的基本路径
+    """
+    folder = os.path.abspath(os.path.join(os.path.abspath(__file__),"..\\..\\"))
+    return folder
 
-def walk_through_files(process_func, folder: str = "", filename_re_exp: str = ".*\.htm*"):
+def walk_through_files(process_func, folder_name: str = "", filename_re_exp: str = ".*\.htm*"):
     """
     遍历给定文件夹下的所有文件
     process_func: 为每个文件执行的方法：process_func(完整文件路径，无后缀文件名)
     folder: 项目内文件夹，默认为整个不全书项目
     filename_re_exp: 文件名的正则表达式判定，默认仅匹配.htm与.html
     """
-    folder = os.path.abspath(os.path.join(os.path.abspath(__file__),"..\\..\\"+folder))
-    if os.path.isfile(folder):
-        filename = os.path.basename(folder)
+    folder_path = get_base_path()
+    if folder_name:
+        folder_path = folder_path+"/"+folder_name
+    if os.path.isfile(folder_path):
+        filename = os.path.basename(folder_path)
         if re.search(filename_re_exp, filename):
-            process_func(folder,os.path.splitext(filename)[0])
+            process_func(folder_path,os.path.splitext(filename)[0])
     else:
-        for filepath,dirnames,filenames in os.walk(folder):
+        for filepath,dirnames,filenames in os.walk(folder_path):
             for filename in filenames:
                 if re.search(filename_re_exp, filename):
                     file_full_path = os.path.join(filepath,filename)
